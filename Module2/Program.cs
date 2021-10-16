ac3db62110aba34127d7857cb9d44b12086d85b3
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-10-16 14:17:07 +0530

    Add files via upload

---
+++ Module2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello"); // declaration and initialization
+
+            sb.Append(" World"); // appending with another word
+            Console.WriteLine(sb);
+
+            sb.Insert(5, "c#"); // inserting a new word
+            Console.WriteLine("Inserted String: " + sb);
+
+            sb.Replace("c#", " "); //replacing a word
+            Console.WriteLine(sb);
+
+            sb.Remove(5, 3); // removes 3 characters from index 5
+            Console.WriteLine(sb);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
