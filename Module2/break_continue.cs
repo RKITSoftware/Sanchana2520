e9b64fd96be16c54f55424a1c4e2292a2d5190d1
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-10-16 14:26:45 +0530

    Add files via upload

---
+++ Module2/break_continue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <= 10; i++)
+            {
+
+                if (i == 5)
+
+                    continue; //jumps out of the current loop condition and jumps back to the starting of the loop code.
+
+                if (i == 7)
+
+                    break;// breaks out of the loop at the current point
+
+                Console.WriteLine(i); //printing the value till 6
+
+            } 
+            Console.ReadLine();     
+        }
+    }
+}
