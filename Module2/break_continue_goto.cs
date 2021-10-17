333a0e204fe8ff5e2db51bad4808458acc53a40c
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-10-17 21:14:07 +0530

    Add files via upload

---
+++ Module2/break_continue_goto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_continue_goto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <= 10; i++)
+            {
+
+                if (i == 5)
+
+                    continue; //jumps out of the current loop condition and jumps back to the starting of the loop code.
+
+                if (i == 7)
+
+                    break;// breaks out of the loop at the current point
+
+                Console.WriteLine(i); //printing the value till 6
+
+            }
+
+            int choice = 3;
+
+            switch (choice)
+            {
+
+                case 1:
+                    Console.WriteLine("case 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("case 2");
+                    break;
+
+                case 3:
+
+                    goto case 1;
+                    break;
+            }
+            Console.ReadLine();     
+        }
+    }
+}
