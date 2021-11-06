a752d80c7ef7ede1ef82e12b7bb579f13796dbe6
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-11-06 22:19:10 +0530

    Add files via upload

---
+++ Module3/event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace event_demo
+{
+    public delegate string demoDelegate(string str1, string str2); //delegate
+
+    class event_prog
+
+    {   event demoDelegate myEvent; //creating event
+
+        public event_prog() 
+        {
+        this.myEvent += new demoDelegate(this.Display);
+     }
+
+     public string Display(string studentname, string subject)
+     {
+        return "Student: " + studentname + "\nSubject: " +subject;
+     }
+     static void Main(string[] args)
+     {
+
+         event_prog obj = new event_prog(); //creating object
+         string display = obj.myEvent("Sanchana", "Science");
+         Console.WriteLine("Detail:" + display); //displaying detail
+     }
+    }
+}
