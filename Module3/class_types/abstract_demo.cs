9e0e161aebb1f5c42a0ebb0bd28aeb7e83ce5d19
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-11-06 22:34:32 +0530

    Add files via upload

---
+++ Module3/class_types/abstract_demo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract__demo
+{
+    public abstract class Shape
+    {
+        public abstract void draw();
+    }
+    public class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }  
+        static void Main(string[] args)
+        {
+            objShape obj;
+            obj = new Rectangle();
+            obj.draw();
+            obj = new Circle();
+            obj.draw();  
+        }
+    }
+}
