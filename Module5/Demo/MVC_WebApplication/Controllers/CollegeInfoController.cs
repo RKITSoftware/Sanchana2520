988d7ce1c7774be416b8bb9d7d7b430458b272f2
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-12-01 21:46:41 +0530

    Add files via upload

---
+++ Module5/Demo/MVC_WebApplication/Controllers/CollegeInfoController.cs
@@ -0,0 +1,25 @@
+﻿using MVC_WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_WebApplication.Controllers
+{
+    public class CollegeInfoController : Controller
+    {
+        // GET: CollegeInfo
+        public ActionResult DisplayData()
+        {
+            CollegeInfo obj = new CollegeInfo(); //creating object
+
+            //creating list 
+            obj.ListOfCollege();
+
+            //passing the content to view
+            return View(obj);
+           
+        }
+    }
+}
---
+++ Module5/Demo/MVC_WebApplication/Models/CollegeInfo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WebApplication.Models
+{
+    public class College
+    {
+        public int sr_no;
+        public String Name;
+        public String City;
+
+    }
+    public class CollegeInfo
+    {//create a list
+        public List<College> listCollege { get; set; }
+
+        //list method
+        public void ListOfCollege()
+        {
+            listCollege = new List<College>();
+
+            //creating object and adding information
+            College obj_College = new College();
+            obj_College.sr_no = 1;
+            obj_College.Name = "Atmiya University";
+            obj_College.City = "Rajkot";
+
+            //add colleges to the list
+            listCollege.Add(obj_College);
+
+            //Adding another detail
+            obj_College = new College();
+            obj_College.sr_no = 1;
+            obj_College.Name = "Atmiya University";
+            obj_College.City = "Rajkot";
+
+            //adding more details
+            listCollege.Add(obj_College);
+
+            obj_College = new College();
+            obj_College.sr_no = 2;
+            obj_College.Name = "GEC";
+            obj_College.City = "Rajkot";
+
+            listCollege.Add(obj_College);
+
+            obj_College = new College();
+            obj_College.sr_no = 3;
+            obj_College.Name = "Marwadi";
+            obj_College.City = "Rajkot";
+
+            listCollege.Add(obj_College);
+
+
+
+
+        }
+    }
+}
