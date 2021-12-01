244d27be9e3a48d0a2d8d6bcf950308380f46abe
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2021-12-01 21:51:45 +0530

    Add files via upload

---
+++ Module5/Demo/Binary Serialization Demo/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Binary_Serialization_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCollege = new System.Windows.Forms.TextBox();
+            this.serialize = new System.Windows.Forms.Button();
+            this.deserialize = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(118, 30);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "City";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(118, 74);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCity.TabIndex = 3;
+            this.textBoxCity.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "College";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxCollege
+            // 
+            this.textBoxCollege.Location = new System.Drawing.Point(118, 117);
+            this.textBoxCollege.Name = "textBoxCollege";
+            this.textBoxCollege.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCollege.TabIndex = 5;
+            // 
+            // serialize
+            // 
+            this.serialize.Location = new System.Drawing.Point(100, 178);
+            this.serialize.Name = "serialize";
+            this.serialize.Size = new System.Drawing.Size(75, 23);
+            this.serialize.TabIndex = 6;
+            this.serialize.Text = "serialize";
+            this.serialize.UseVisualStyleBackColor = true;
+            // 
+            // deserialize
+            // 
+            this.deserialize.Location = new System.Drawing.Point(100, 207);
+            this.deserialize.Name = "deserialize";
+            this.deserialize.Size = new System.Drawing.Size(75, 23);
+            this.deserialize.TabIndex = 7;
+            this.deserialize.Text = "deserialize";
+            this.deserialize.UseVisualStyleBackColor = true;
+            this.deserialize.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(118, 152);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Date of birth";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "msg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.serialize);
+            this.Controls.Add(this.deserialize);
+            this.Controls.Add(this.textBoxCollege);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+       
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCollege;
+        private System.Windows.Forms.Button serialize;
+        private System.Windows.Forms.Button deserialize;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+   
+    }
+}
+
---
+++ Module5/Demo/Binary Serialization Demo/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binary_Serialization_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Serialize_Click(object sender, EventArgs e)
+        {
+           Student obj = new Student
+            {
+                name =textBoxName.Text,
+                city = textBoxCity.Text,
+                college = textBoxCollege.Text,
+                dob = dateTimePicker1.Value,
+               
+            };
+
+            //BinaryFormatter to serialize the object in BinaryFormat.
+            BinaryFormatter obj_bf = new BinaryFormatter();
+
+            //Make a file using FileStream 
+            FileStream obj_out = new FileStream("student.binary", FileMode.Create, FileAccess.Write, FileShare.None);
+            try
+            {
+                using (obj_out)
+                {
+                    //bf.Serialize(obj_out, emp) will serialize the object "obj" and store it in file "employee.binary"
+                    obj_bf.Serialize(obj_out, obj);
+                    label5.Text = "Object has been Serialized";
+                }
+            }
+            catch
+            {
+                label5.Text = "Error";
+            }
+        }
+
+        //click event for Deserialize button. 
+        //When clicking on the Deserialize button,deserialize the object and show its values on screen
+        private void Deserialize_Click(object sender, EventArgs e)
+        {
+            Student obj1 = new Student();
+
+            //BinaryFormatter to serialize the object in BinaryFormat.
+            BinaryFormatter obj_bf = new BinaryFormatter();
+
+            FileStream obj_in = new FileStream("student.binary", FileMode.Open, FileAccess.Read, FileShare.None);
+            try
+            {
+                using (obj_in)
+                {
+                    //deserialize the object
+                    obj1 = (Student)obj_bf.Deserialize(obj_in);
+                    label5.Text = "Object Deserialized";
+
+                    textBoxName.Text = obj1.name;
+                    textBoxCity.Text = obj1.city;
+                    dateTimePicker1.Value = obj1.dob;
+                }
+            }
+            catch
+            {
+                label5.Text = "Error";
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
