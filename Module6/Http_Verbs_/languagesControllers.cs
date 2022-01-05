304806608c00a08db963e4f8f226729836145a1c
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2022-01-05 10:34:00 +0530

    Update and rename Module6/Http_Verbs_/Controllers/ValuesController.cs to Module6/Http_Verbs_/languagesControllers.cs

--- Module6/Http_Verbs_/languagesControllers.cs
+++ Module6/Http_Verbs_/languagesControllers.cs
@@ -10,26 +10,28 @@ namespace Http_Verbs_.Controllers
     [RoutePrefix("api/values")]
     public class ValuesController : ApiController
     {
+        //creating list
         static List<string> languages = new List<string>() {   
             "C#","ASP.NET","MVC"  
               }; 
 
-        // GET api/values
-        [Route("")]  //This gives result to GET method without parameter
+
+        [Route("")]  
+        //This gives result to GET method without parameter
         public IEnumerable<string> Get()
         {
             return languages; 
            // return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+       
         [Route("{int:id}")]
         public string Get(int id)
         {
             return languages[id];  
         }
 
-        // POST api/values
+       
         //used to create new item
         [Route("")]
         public void Post([FromBody]string value)
@@ -38,15 +40,17 @@ namespace Http_Verbs_.Controllers
             languages.Add(value); 
         }
 
-        // PUT api/values/5
-        [Route("int:id")]  //used to update an item
+        
+        [Route("int:id")] 
+        //used to update an item
         public void Put(int id, [FromBody]string value)
         {
             languages[id] = value;  
         }
 
-        // DELETE api/values/5
-        [Route("int:id")]    //used to remove an item
+       
+        [Route("int:id")]   
+        //used to remove an item
          [HttpDelete]
         public void Delete(int id)
         {
