b1bd0fe9340230aa7bd1ac7389dfaa7e66728e93
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2022-01-05 10:44:13 +0530

    Add files via upload

---
+++ Module7/WebApi_demo/Models/Student_info.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_demo.Models
+{
+    public class Student_info
+    {
+        #region Public variables
+
+        /// <summary>
+        /// primary key StudentID 
+        /// </summary>
+        [Key]
+        public int StudentID { get; set; }
+
+        /// <summary>
+        /// Field for StudentName 
+        /// </summary>
+       
+        public string StudentName { get; set; }
+
+        /// <summary>
+        /// Field for Subject
+        /// </summary>
+        public string Subject { get; set; }
+    }
+}
