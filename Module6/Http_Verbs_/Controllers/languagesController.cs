50d0bdd4c8c58101b10beac7436c4e836d001aa6
Sanchana2520 <90302152+Sanchana2520@users.noreply.github.com>
2022-01-05 11:24:49 +0530

    Update languagesController.cs

--- Module6/Http_Verbs_/Controllers/languagesController.cs
+++ Module6/Http_Verbs_/Controllers/languagesController.cs
@@ -15,7 +15,7 @@ namespace Http_Verbs_.Controllers
             "C#","ASP.NET","MVC"  
               }; 
 
-       
+      
         [Route("")] 
         //This gives result to GET method without parameter
         public IEnumerable<string> Get()
@@ -23,15 +23,25 @@ namespace Http_Verbs_.Controllers
             return languages; 
         
         }
-
+        
+        /// <summary>
+        /// Get()
+        /// method return List laguages
+        /// which would contain the elements in list
+        /// </summary>
+        /// <returns>List of type languages</returns>
         [Route("{int:id}")]
         public string Get(int id)
         {
             return languages[id];  
         }
 
-        
-        //used to create new item
+        /// <summary>
+        /// method to add languages in list
+        /// </summary>
+        /// <param name="id">
+        /// which contains list of languages
+        /// </param>
         [Route("")]
         public void Post([FromBody]string value)
         {
@@ -40,14 +50,27 @@ namespace Http_Verbs_.Controllers
         }
 
         
+        /// <summary>
+        /// method to edit list
+        /// </summary>
+        /// <param name="id">
+        /// id would be taken from URI and then value is taken from body
+        /// </param>
         [Route("int:id")] 
         //used to update an item
-
         public void Put(int id, [FromBody]string value)
         {
             languages[id] = value;  
         }
 
+        
+        /// <summary>
+        /// Delete(int id)
+        /// method to remove item from the list
+        /// id would be matched using the id given by the user
+        /// where, id is taken from URI
+        /// </summary>
+        /// <param name="id">id is unique identity of language to be deleted</param>
         [Route("int:id")]   
         //used to remove an item
          [HttpDelete]
